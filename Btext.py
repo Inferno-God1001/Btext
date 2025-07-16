1ff9245b5086baaa51f684aad53bc6e339d5db4d
DimenCoder <renangameslinha8@gmail.com>
2025-07-16 18:14:57 -0300

    Atualizar o Btext.py

--- Btext.py
+++ Btext.py
@@ -1,9 +1,10 @@
 import builtins
 
 _original_print = builtins.print
+_original_input = builtins.input
 
 class ColorPrint:
-      
+    
     COLORS = {
         'black': '\033[30m',
         'red': '\033[31m',
@@ -21,5 +22,18 @@ class ColorPrint:
         color_code = cls.COLORS.get(color.lower(), cls.COLORS['white'])
         text = ' '.join(str(arg) for arg in args)
         _original_print(f"{color_code}{text}{cls.COLORS['reset']}", **kwargs)
-        
+    
+    @classmethod
+    def input(cls, prompt='', color='white'):
+        """Substitui a função input padrão com suporte a cores"""
+        color_code = cls.COLORS.get(color.lower(), cls.COLORS['white'])
+        colored_prompt = f"{color_code}{prompt}{cls.COLORS['reset']}"
+        return _original_input(colored_prompt)
+    
+    @classmethod
+    def restore_all(cls):
+        builtins.print = _original_print
+        builtins.input = _original_input
+
 builtins.print = ColorPrint.print
+builtins.input = ColorPrint.input
